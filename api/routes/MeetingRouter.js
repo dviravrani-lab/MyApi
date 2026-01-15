7047da4a005a031189a7c3a6ba43c9888f4948db
dviravrani-lab <dviravrani@gmail.co>
2026-01-15 09:30:25 +0200

    Remove files that should be ignored

--- api/routes/MeetingRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {getMeetings ,createMeeting ,deleteMeeting} = require("../controllers/MeetingController");
-
-router.get("/:userId", getMeetings);
-router.post("/", createMeeting);
-router.delete("/:id", deleteMeeting);
-
-module.exports = router;
